297903b1dd4828769d1b3bcd449c1be186f97aa8
artshar <sharapudinov@mial.ru>
2016-04-11 01:04:10 +0300

    breadcrumbs schema

--- www/bitrix/templates/dresscode/components/bitrix/breadcrumb/.default/template.php
+++ www/bitrix/templates/dresscode/components/bitrix/breadcrumb/.default/template.php
@@ -4,7 +4,7 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 if(empty($arResult))
 	return "";
 	
-$strReturn = '<div id="breadcrumbs"><ul>';
+$strReturn = '<div id="breadcrumbs"><ul vocab="http://schema.org/" typeof="BreadcrumbList">';
 
 $num_items = count($arResult);
 for($index = 0, $itemSize = $num_items; $index < $itemSize; $index++)
@@ -12,9 +12,9 @@ for($index = 0, $itemSize = $num_items; $index < $itemSize; $index++)
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
 	
 	if($arResult[$index]["LINK"] <> "" && $index != $itemSize-1)
-		$strReturn .= '<li><a href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a></li><li><span class="arrow"> &bull; </span></li>';
+		$strReturn .= '<li  property="itemListElement" typeof="ListItem"><a  property="item" typeof="WebPage" href="'.$arResult[$index]["LINK"].'" title="'.$title.'">'.$title.'</a><meta property="position" content="'.($index+1).'"></li><li><span class="arrow"> &bull; </span> </li>';
 	else
-		$strReturn .= '<li><span class="changeName">'.$title.'</span></li>';
+		$strReturn .= '<li  property="itemListElement" typeof="ListItem"><span property="name" class="changeName">'.$title.'</span> <meta property="position" content="'.($index+1).'"></li>';
 }
 
 $strReturn .= '</ul></div>';
