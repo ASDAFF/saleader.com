2e1c4abb0b8b391267ac7f2cf39eb40a6b4bee11
artshar <sharapudinov@mail.ru>
2017-03-13 19:41:26 +0300

    logo footer fix

--- www/sect_footer_left.php
+++ www/sect_footer_left.php
@@ -1,7 +1,7 @@
 <div class="logo">
 	<?if(MAIN_PAGE === TRUE):?>
-		<span><img src="<?=SITE_TEMPLATE_PATH?>/images/logo.png" alt=""></span>
+		<span><img src="<?=SITE_TEMPLATE_PATH?>/images/logo_white_text.png" alt=""></span>
 	<?else:?>
-		<a href="<?=SITE_DIR?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/logo.png" alt=""></a>
+		<a href="<?=SITE_DIR?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/logo_white_text.png" alt=""></a>
 	<?endif;?>
 </div>
